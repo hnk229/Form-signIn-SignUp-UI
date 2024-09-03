b72ae7d428be813176e9d637e7349767c9131307
Ulrich HOUENOUKPO <houenoikpoulrich2@gmail.com>
2024-09-03 07:56:32 +0100

    initiaisation to the project

---
+++ lib/animation/delayed_animation.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+
+class DelayedAnimation extends StatefulWidget {
+  final Widget child;
+  final int delay;
+  const DelayedAnimation({super.key, required this.child, required this.delay});
+
+  @override
+  State<DelayedAnimation> createState() => _DelayedAnimationState();
+}
+
+class _DelayedAnimationState extends State<DelayedAnimation> with SingleTickerProviderStateMixin{
+  late AnimationController _controller; //Animation Controller
+  late Animation<Offset> _animOffset;
+  @override
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      //Animation Duration
+      duration: const Duration(milliseconds: 800),
+    );
+
+    final curve = CurvedAnimation(
+        parent: _controller,
+        curve: Curves.decelerate
+    );
+
+    //An
+    _animOffset = Tween<Offset>(
+      begin: const Offset(0.0, 0.35),
+      end: Offset.zero,
+    ).animate(curve);
+
+    Timer(Duration(milliseconds: widget.delay), (){
+      _controller.forward();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+        opacity: _controller,
+      child: SlideTransition(
+        position: _animOffset,
+        child: widget.child,
+      ),
+    );
+  }
+}
